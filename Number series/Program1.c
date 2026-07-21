d2eb615dba8812b98c7ac6bcc96e60b21ea9511e
kannanbrindha77-lang <kannanbrindha77@gmail.com>
2026-07-21 10:18:33 +0530

    Create Program1.c

---
+++ Number series/Program1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{int a,b,sum;
+printf("Enter the first Number:");
+scanf ("%d",&a);
+printf("Enter the second Number:");
+scanf("%d",&b);
+sum=a+b; 
+printf("sum=%d",sum);
+return 0;
+}
