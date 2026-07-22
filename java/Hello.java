940bcf674efe3bb6f69b8c6f100d42e69454f5ca
kannanbrindha77-lang <kannanbrindha77@gmail.com>
2026-07-22 20:33:07 +0530

    Delete java directory

--- java/Hello.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner
-  public class Main{
-    public static void main(String[]args){
-      scanner.sc = new scanner(system.in);
-      system.out.print=("Hello World")
-    }
-  }
-  
--- java/Main.java
+++
@@ -1,8 +0,0 @@
-import java.utill.scanner;
-public class Main{
-  public static void main(String[]args){
-    scanner.sc = new scanner(system.in);
-    char name = "Brindha";
-    System.out.print("My name is + name")
-  }
-}
