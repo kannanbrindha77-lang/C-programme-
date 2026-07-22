1f633ab59de44ef87dabfe30b3dccde1998e420b
kannanbrindha77-lang <kannanbrindha77@gmail.com>
2026-07-22 06:34:16 +0530

    Create Main.java

---
+++ java/Main.java
@@ -0,0 +1,8 @@
+import java.utill.scanner;
+public class Main{
+  public static void main(String[]args){
+    scanner.sc = new scanner(system.in);
+    char name = "Brindha";
+    System.out.print("My name is + name")
+  }
+}
