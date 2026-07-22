6d85cb329d5b1e0ce94b07437a62ca8ff557387b
kannanbrindha77-lang <kannanbrindha77@gmail.com>
2026-07-22 06:16:19 +0530

    Create Hello.java

---
+++ java/Hello.java
@@ -0,0 +1,8 @@
+import java.util.Scanner
+  public class Main{
+    public static void main(String[]args){
+      scanner.sc = new scanner(system.in);
+      system.out.print=("Hello World")
+    }
+  }
+  
